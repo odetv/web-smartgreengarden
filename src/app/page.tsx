4e68b9be839e01525aceee3108d0fdd7b09f1aa3
odetv <abdiutama02@gmail.com>
2024-06-04 21:15:12 +0800

    initial project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,111 +1,191 @@
+import {
+  Button,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
 import Image from "next/image";
+import LogoUndiksha from "@/assets/images/logo/LogoUndiksha.png";
+import LogoDagoEngineering from "@/assets/images/logo/LogoDagoEng.png";
+import LogoCGPDagoEng from "@/assets/images/logo/LogoCGPDagoEng.png";
+import LogoDelectra from "@/assets/images/logo/LogoDelectra.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="flex min-h-screen flex-col items-center justify-center pt-10 sm:pt-0 gap-10 w-10/12 mx-auto">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <p className="font-bold text-2xl lg:text-6xl text-center">
+          Smart Green Garden
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <p className="text-sm text-center">
+          Fakultas Teknik dan Kejuruan Universitas Pendidikan Ganesha melakukan
+          kerja sama kolaborasi bersama dengan PT. Dago Engineering, Bandung.
+          Sebagai bentuk penggunaan teknologi, MBKM ini mengusung tema Smart
+          Green Garden yang berfokus pada sektor pertanian hidroponik dan sektor
+          teknologi canggih yaitu Internet of Things (IoT), Artificial
+          Intelligence (AI), dan pemanfaatan Energi Terbarukan dari sinar
+          matahari menggunakan panel surya.
+        </p>
+      </div>
+      <div className="mt-2 flex flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row justify-center items-center gap-6">
+        <div className="flex flex-row justify-center items-center gap-6">
+          <Popover placement="bottom" offset={10} showArrow>
+            <PopoverTrigger>
+              <div>
+                <Image
+                  width={80}
+                  height={80}
+                  style={{ height: "auto", width: 72 }}
+                  className="transform hover:scale-105 transition-transform-opacity object-cover"
+                  alt="Undiksha"
+                  src={LogoUndiksha.src}
+                  priority={true}
+                />
+              </div>
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className="px-1 py-2 text-center">
+                <div className="text-small font-bold">Undiksha</div>
+                <div className="text-tiny">Fakultas Teknik dan Kejuruan</div>
+                <Button
+                  size="sm"
+                  color="primary"
+                  variant="flat"
+                  className="mt-1"
+                >
+                  <a target="_blank" href="https://undiksha.ac.id/">
+                    Kunjungi
+                  </a>
+                </Button>
+              </div>
+            </PopoverContent>
+          </Popover>
+          <Popover placement="bottom" offset={10} showArrow>
+            <PopoverTrigger>
+              <div>
+                <Image
+                  width={172}
+                  height={172}
+                  style={{ height: "auto", width: 172 }}
+                  className="transform hover:scale-105 transition-transform-opacity object-cover"
+                  alt="PT. Dago Engineering"
+                  src={LogoDagoEngineering.src}
+                  priority={true}
+                />
+              </div>
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className="px-1 py-2 text-center">
+                <div className="text-small font-bold">PT. Dago Engineering</div>
+                <div className="text-tiny">Bandung</div>
+                <Button
+                  size="sm"
+                  color="primary"
+                  variant="flat"
+                  className="mt-1"
+                >
+                  <a target="_blank" href="https://dagoeng.co.id/">
+                    Kunjungi
+                  </a>
+                </Button>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="flex flex-row justify-center items-center gap-6">
+          <Popover placement="bottom" offset={10} showArrow>
+            <PopoverTrigger>
+              <div>
+                <Image
+                  width={162}
+                  height={162}
+                  style={{ height: "auto", width: 162 }}
+                  className="transform hover:scale-105 transition-transform-opacity object-cover"
+                  alt="Clean & Green Power"
+                  src={LogoCGPDagoEng.src}
+                  priority={true}
+                />
+              </div>
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className="px-1 py-2 text-center">
+                <div className="text-small font-bold">Clean & Green Power</div>
+                <div className="text-tiny">Bandung</div>
+                <Button
+                  size="sm"
+                  color="primary"
+                  variant="flat"
+                  className="mt-1"
+                >
+                  <a target="_blank" href="https://dagoeng.co.id/">
+                    Kunjungi
+                  </a>
+                </Button>
+              </div>
+            </PopoverContent>
+          </Popover>
+          <Popover placement="bottom" offset={10} showArrow>
+            <PopoverTrigger>
+              <div>
+                <Image
+                  width={238}
+                  height={238}
+                  style={{ height: "auto", width: 238 }}
+                  className="transform hover:scale-105 transition-transform-opacity object-cover"
+                  alt="Delectra"
+                  src={LogoDelectra.src}
+                  priority={true}
+                />
+              </div>
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className="px-1 py-2 text-center">
+                <div className="text-small font-bold">Delectra</div>
+                <div className="text-tiny">Bandung</div>
+                <Button
+                  size="sm"
+                  color="primary"
+                  variant="flat"
+                  className="mt-1"
+                >
+                  <a target="_blank" href="https://delectra.id/">
+                    Kunjungi
+                  </a>
+                </Button>
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-2 gap-4">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://web-nextgenhydroponics.vercel.app/"
+          className="bg-emerald-100 group rounded-xl border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
+          <h2 className="mb-1 sm:text-xl font-bold p-2">
+            Next-Gen Hydroponics{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
+          <p className="text-xs opacity-50">Kunjungi Project Tim 1.</p>
         </a>
-
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://hydrohealth.vercel.app/"
+          className="bg-emerald-100 group rounded-xl border border-transparent px-5 py-4 transition-colors hover:border-gray-100 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
+          <h2 className="mb-1 sm:text-xl font-bold p-2">
+            Hydrohealth Project{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p className="text-xs opacity-50">Kunjungi Project Tim 2.</p>
         </a>
       </div>
     </main>
